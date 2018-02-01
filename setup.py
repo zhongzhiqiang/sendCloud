818fca33c4211f5163059c4c1b92fdbd52dbccc1
zhongzhiqiang <200730249@qq.com>
2018-02-01 10:29:06 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+PACKAGE = "sendCloud"
+NAME = "sendCloud"
+DESCRIPTION = "send cloud web api"
+AUTHOR = "zhongzq"
+VERSION = '0.1'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    license="BSD",
+    packages=[PACKAGE],
+    install_requires=[
+        'requests==2.12.4',
+        'voluptuous==0.9.3',
+    ],
+    zip_safe=False,
+)
